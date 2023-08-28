1334a70fa663100ba8a3d854c55b8ecdf83da65f
起汐 Yuics <yuuuics0@gmail.com>
2023-08-28 22:01:17 +0800

    UOJ #1

---
+++ UOJ/1/1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+typedef long long i64;
+
+void solve() {
+	i64 a, b;
+	std::cin >> a >> b;
+	std::cout << a + b << std::endl;
+}
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr), std::cout.tie(nullptr);
+
+	solve();
+	return 0;
+}
