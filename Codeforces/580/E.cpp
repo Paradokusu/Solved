306aa23d1d0c1b05cb0ae73cd610d441f6a56bce
Yuics <yuicsmoe@outlook.com>
2024-06-14 14:17:47 +0800

    CF 580 E

--- Codeforces/580/E.cpp
+++ Codeforces/580/E.cpp
@@ -16,13 +16,13 @@ struct node {
 };
 
 struct seg {
-    node tr[N << 8];
+    node tr[N << 2];
 
     #define ls o << 1
     #define rs o << 1 | 1
 
     void pushup(int o) {
-        tr[o].w = ((tr[ls].w * pw[tr[rs].r - tr[ls].l + 1]) % MOD + tr[rs].w % MOD) % MOD;
+        tr[o].w = ((tr[ls].w * pw[tr[rs].r - tr[rs].l + 1]) % MOD + tr[rs].w % MOD) % MOD;
     }
 
     void pushdown(int o) {
@@ -31,7 +31,7 @@ struct seg {
         tr[ls].w = val[tg][tr[ls].r - tr[ls].l + 1];
         tr[rs].w = val[tg][tr[rs].r - tr[rs].l + 1];
         tr[ls].tag = tr[rs].tag = tg;
-        tr[o].tg = -1;
+        tr[o].tag = -1;
     }
 
     void build(int o, int l, int r) {
@@ -55,8 +55,8 @@ struct seg {
         pushdown(o);
         int mid = (tr[o].l + tr[o].r) >> 1;
         if (mid >= r) update(ls, l, r, x);
-        else if (l < mid) update(rs, l, r, x);
-        else { update(ls, l, mid, x); update(rs, mid + 1, r, k); }
+        else if (l > mid) update(rs, l, r, x);
+        else { update(ls, l, mid, x); update(rs, mid + 1, r, x); }
         pushup(o);
     }
 
@@ -64,14 +64,19 @@ struct seg {
         if (tr[o].l == l && tr[o].r == r) return tr[o].w;
         pushdown(o);
         int mid = (tr[o].l + tr[o].r) >> 1;
-        if (r <= mid) return query(ls, l, r);
+        if (mid >= r) return query(ls, l, r);
         else if (l > mid) return query(rs, l, r);
-        else ((((query(ls, l, mid) % MOD) * pw[r - mid]) % MOD) + (query(rs, mid + 1, r) % MOD)) % MOD;
+        else return ((((query(ls, l, mid) % MOD) * pw[r - mid]) % MOD) + (query(rs, mid + 1, r) % MOD)) % MOD;
     }
 } ts;
 
 void init() {
     pw[0] = 1;
+    for (int i = 1; i < N; i++)
+        pw[i] = pw[i - 1] * BASE % MOD;
+    for (int i = 0; i < 10; i++)
+        for (int j = 1; j < N; j++)
+            val[i][j] = ((val[i][j - 1] * BASE) % MOD + i) % MOD;
 }
 
 int main() {
@@ -79,4 +84,24 @@ int main() {
     scanf(" %s", a + 1);
 
     init();
+    int q = m + k;
+
+    ts.build(1, 1, n);
+    while (q--) {
+        int opt, l, r, k;
+        scanf("%d %d %d %d", &opt, &l, &r, &k);
+        if (opt == 1) {
+            ts.update(1, l, r, k);
+        } else {
+            if (r - l + 1 == k) {
+                printf("YES\n");
+                continue;
+            }
+            if (ts.query(1, l, r - k) == ts.query(1, l + k, r))
+                printf("YES\n");
+            else printf("NO\n");
+        }
+    }
+
+    return 0;
 }
